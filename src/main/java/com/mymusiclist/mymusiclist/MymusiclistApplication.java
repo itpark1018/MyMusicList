93c69248c4fe64a427883a46e23e3f3f5005bdaf
itpark1018 <itpark1018@gmail.com>
2024-01-12 20:17:44 +0900

    first

---
+++ src/main/java/com/mymusiclist/mymusiclist/MymusiclistApplication.java
@@ -0,0 +1,13 @@
+package com.mymusiclist.mymusiclist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymusiclistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymusiclistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mymusiclist/mymusiclist/MymusiclistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymusiclist.mymusiclist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymusiclistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
