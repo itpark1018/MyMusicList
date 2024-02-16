84000e95a5df368f4693c88a4a7dbd1503795a3b
itpark1018 <itpark1018@gmail.com>
2024-02-17 04:26:32 +0900

    DeleteRequest

--- src/main/java/com/mymusiclist/backend/music/dto/request/DeleteRequest.java
+++ src/main/java/com/mymusiclist/backend/music/dto/request/DeleteRequest.java
@@ -1,5 +1,6 @@
 package com.mymusiclist.backend.music.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,5 +15,6 @@ import lombok.Setter;
 @Builder
 public class DeleteRequest {
 
+  @NotNull(message = "노래 식별자는 NULL 일 수 없습니다.")
   private List<Long> musicId;
 }
